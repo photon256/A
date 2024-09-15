10b1d1cc3600af08d1aa6096e2f8d205d597b065
photon256 <144157217+photon256@users.noreply.github.com>
2024-09-15 10:42:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config(object):
     # get a token from @BotFather
     pass
     """
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7519957401:AAGF61wj0nC0OlbipiT2Awcy7q9o5ZeEBBA")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6722382064:AAES1OLvEfL5HLTgXNZDsgpSrFWFfgmHuV8")
     API_ID = int(os.environ["API_ID", 16253557]
     API_HASH = os.environ["API_HASH", "81171c25e4cb9062cb10da8b7730432a"]
     AUTH_USERS = "1996039956"""
